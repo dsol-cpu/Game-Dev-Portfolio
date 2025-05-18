9309980a117040bf2a6863fadbd5a4f038305521
dsol-cpu <dsolinsky98@gmail.com>
2025-05-18 11:58:04 -0400

    separated blog data from blog file

--- src/front_end/src/js/blog.js
+++ src/front_end/src/js/blog.js
@@ -1,26 +1,4 @@
-import fallback from "/images/you_can_do_it.jpeg";
-import cupil from "/images/cupil.png";
-const BlogTag = Object.freeze({
-  GENERAL: "General",
-  REAL: "Real",
-});
-
-// Blog post data
-const blogPostsData = [
-  {
-    tags: [BlogTag.GENERAL],
-    title: "I am Lorem",
-    description:
-      "Of the ipsumums.  Of all the love and the glory and the heaven and the forever and yeah and um and so um yeah thanks for coming to my um hmmmm i forgor skull emoji.",
-    thumbnail: cupil,
-  },
-  {
-    tags: [BlogTag.REAL],
-    title: "Chiikawa Motivational Poster",
-    description: "You can do it.",
-    thumbnail: fallback,
-  },
-];
+import { blogPostsData } from "./data/blog";
 
 /**
  * Create a blog post item
---
+++ src/front_end/src/js/data/blog.js
@@ -0,0 +1,24 @@
+import fallback from "/images/you_can_do_it.jpeg";
+import cupil from "/images/cupil.png";
+
+const BlogTag = Object.freeze({
+  GENERAL: "General",
+  REAL: "Real",
+});
+
+// Blog post data
+export const blogPostsData = [
+  {
+    tags: [BlogTag.GENERAL],
+    title: "I am Lorem",
+    description:
+      "Of the ipsumums.  Of all the love and the glory and the heaven and the forever and yeah and um and so um yeah thanks for coming to my um hmmmm i forgor skull emoji.",
+    thumbnail: cupil,
+  },
+  {
+    tags: [BlogTag.REAL],
+    title: "Chiikawa Motivational Poster",
+    description: "You can do it.",
+    thumbnail: fallback,
+  },
+];
